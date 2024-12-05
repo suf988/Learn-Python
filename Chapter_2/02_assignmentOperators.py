8b6ed5488249dd63167ca48133d85e2f630a0fea
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-05 16:13:47 +0100

    learning variables and datatypes

--- Chapter_2/02_assignmentOperators.py
+++ Chapter_2/02_assignmentOperators.py
@@ -8,6 +8,6 @@ print (c)
 
 initialValue = 5
 initialValue += 4   #increment of 4 in the initial value
-#initialValue -= 4   #decrement of 4 in the initial value
+initialValue -= 4   #decrement of 4 in the initial value
 
 print(initialValue)
