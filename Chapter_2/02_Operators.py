4716cd07dc7bbef3d93366256b6463ffaf73b1e8
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-05 16:18:27 +0100

    changed the named of the directory

---
+++ Chapter_2/02_Operators.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+c = a+b
+
+print (c)
+
+# +++++++++++++++++++++++++++++
+
+initialValue = 5
+initialValue += 4   #increment of 4 in the initial value
+initialValue -= 4   #decrement of 4 in the initial value
+
+print(initialValue)
