504c2121ffb43dd93b22dbdc241e939729a4b54f
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-05 16:25:40 +0100

    learning comparison operators

--- Chapter_2/02_Operators.py
+++ Chapter_2/02_Operators.py
@@ -1,3 +1,4 @@
+# ASSIGNMENT OPERATOR
 a = 1
 b = 2
 c = a+b
@@ -11,3 +12,15 @@ initialValue += 4   #increment of 4 in the initial value
 initialValue -= 4   #decrement of 4 in the initial value
 
 print(initialValue)
+
+
+# COMPARISON OPERATOR
+num_1 = 15
+num_2 = 19
+
+print(num_1 > num_2)  #False
+print(num_1 < num_2)  #True
+print(num_1 >= num_2)  #False
+print(num_1 <= num_2)  #True
+print(num_1 != num_2)  #True
+print(num_1 == num_2)  #False
