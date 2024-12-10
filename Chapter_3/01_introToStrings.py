4e3d09db714138111331c6aa11c7a0fdd3259883
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 15:50:03 +0100

    learning slicing strings

--- Chapter_3/01_introToStrings.py
+++ Chapter_3/01_introToStrings.py
@@ -4,9 +4,15 @@
 
 string1 = "Harry Potter"
 
-print(string1[6])   #slicing a character by "stringName[indexOfCharacter]"
+print("Slicing a character: ",string1[6])   #slicing a character by "stringName[indexOfCharacter]"
 
-print(string1[0:7]) #slicing a string by "stringName[startingIndex:endingIndex]" where ending index wont be counted
+print("Slicing a string: ",string1[0:7]) #slicing a string by "stringName[startingIndex:endingIndex]" where ending index wont be counted
+
+print("Slicing without giving first parameter: ",string1[:9])  #pehla parameter khali hai mtlb 0 se start ho rha hai
+
+print("Slicing without giving last parameter: ",string1[2:])  #ankhri parameter khali hai mtlb length-1 tk slice hoga
+
+print("Slicing without giving both parameters: ",string1[:])  #dono parameter khali hai mtlb 0 se start ho rha hai aur length-1 tk slice hoga
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # NEGATIVE SLICING
@@ -15,9 +21,8 @@ print(string1[0:7]) #slicing a string by "stringName[startingIndex:endingIndex]"
 # normal length (positive length) 0 se start hoti hai LEFT TO RIGHT
 # negative length -1 se start hoti hai from RIGHT TO LEFT
 
-
-
 name = "Hermione Granger"
 
-print(name[-13:-5]) #-13 length se start hui slicing aur -5 se phle wali length tk slicing ki
+print("Negative Slicing: ",name[-13:-5]) #-13 length se start hui slicing aur -5 se phle wali length tk slicing ki
+
 print(name[-5:-13]) #this would be wrong because starting index should be less than ending index
