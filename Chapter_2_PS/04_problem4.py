c86e65f2a7d9fd27aab57621443a67a66fedb465
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 13:19:33 +0100

    solved practice question 4

---
+++ Chapter_2_PS/04_problem4.py
@@ -0,0 +1,12 @@
+# Q4: Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80
+
+a = 34
+b = 80
+
+print(f"Is 34 greater than 80? {a>b}")
+
+
+# trying by taking user input
+inp1 = int(input("Enter first number: "))
+inp2 = int(input("Enter second number: "))
+print(f"Is {inp1} greater than {inp2}? {inp1>inp2}")
