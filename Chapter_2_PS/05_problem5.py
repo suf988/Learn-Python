1818dbe30d73a4f5f0117e43e14b5bb67030d9ef
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 13:23:13 +0100

    solved practice question 5

---
+++ Chapter_2_PS/05_problem5.py
@@ -0,0 +1,6 @@
+# Q5: Write a python program to find an average of two numbers entered by the user
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"The average of {num1} and {num2} is {(num1+num2)/2}")
