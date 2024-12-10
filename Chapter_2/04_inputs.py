9cb7aea8961d7890fc011b1da76a800c271e4124
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 12:45:13 +0100

    learning to take inputs

--- Chapter_2/04_inputs.py
+++ Chapter_2/04_inputs.py
@@ -8,3 +8,8 @@ c = a+b
 print("Sum of two numbers is: ",c)  #the answer won't be a sum, but both the inputs will be concatenated.
 #REASON: the 'input' key will treat both the inputs as string. 
 # and the '+' symbol concatenates the strings.
+
+inp1 = int(input("Enter first number: "))   #typecasting of the input as 'int'
+inp2 = int(input("Enter second number: "))
+result = inp1 + inp2
+print("The sum of two numbers is: ", result)
