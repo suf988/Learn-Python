bd5468d1bea0b837a679bb3b20af0f359504770e
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 13:29:38 +0100

    solved practice question 6

--- Chapter_2_PS/06_problem6.py
+++ Chapter_2_PS/06_problem6.py
@@ -1,4 +1,4 @@
 # Q6: Write a python program to calculate the square of a number entered by the user.
 
 num = int(input("Enter a number to calculate square: "))
-print(f"The square of number {num} is: {num*num}")
+print(f"The square of number {num} is: {num**2}")
