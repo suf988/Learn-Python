e6423df1065ab97da7b3e489d5f0f46e85a45bef
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 12:52:10 +0100

    solving practice questions

---
+++ Chapter_2_PS/01_problem1.py
@@ -0,0 +1,6 @@
+# Q1: Write a python program to add two numbers
+
+inp1 = int(input("Enter first number: "))
+inp2 = int(input("Enter second number: "))
+
+print("The sum of two numbers is: ", inp1 + inp2)
