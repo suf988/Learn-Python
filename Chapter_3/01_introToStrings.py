1eda251bb93ca3b0bbe2fc04f2d5123f12713699
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 15:26:50 +0100

    learning strings functions

---
+++ Chapter_3/01_introToStrings.py
@@ -0,0 +1,8 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# INTRO TO STRINGS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+string1 = "Harry Potter"
+
+print(string1[6])   #slicing a character by "stringName[indexOfCharacter]"
+print(string1[0:7]) #slicing a string by "stringName[startingIndex:endingIndex]" where ending index wont be counted
