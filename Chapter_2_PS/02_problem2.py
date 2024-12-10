80803dd4bbed1c51fbdc91dfccd4269993937493
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 12:58:46 +0100

    solved practice question 2

---
+++ Chapter_2_PS/02_problem2.py
@@ -0,0 +1,7 @@
+# Q2: Write a python program to find remainder when a number is divided by z.
+
+dividend = int(input("Enter the Dividend: "))
+z = int(input("Enter the Divisor (z): "))
+
+remainder = dividend % z
+print("The remainder of the division is: ", remainder)
