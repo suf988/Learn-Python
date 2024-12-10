b19af565a1e6187b2764c593b32b4554eb2092a8
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-10 16:11:43 +0100

    learning slicing strings

--- Chapter_3/01_introToStrings.py
+++ Chapter_3/01_introToStrings.py
@@ -26,3 +26,33 @@ name = "Hermione Granger"
 print("Negative Slicing: ",name[-13:-5]) #-13 length se start hui slicing aur -5 se phle wali length tk slicing ki
 
 print(name[-5:-13]) #this would be wrong because starting index should be less than ending index
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# SLICING WITH SKIP VALUES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+str2 = "myFavouriteMovieIsHarryPotterAndTheDeathlyHallows"
+
+skippedSlicing = str2[2:39:3]
+print(skippedSlicing) #FoiMisrPtATDt
+
+# REASON
+# ----------
+
+# 1st we slice the string with the first two parameters:
+
+#                     2                                    38
+#                     |                                    |
+#                   myFavouriteMovieIsHarryPotterAndTheDeathlyHallows
+
+# sliced string:    FavouriteMovieIsHarryPotterAndTheDeath
+
+# now taking the 3rd parameter and picking every that number's character:
+
+#                   1  3  3  3  3  3  3  3  3  3  3  3  3 (since the 3rd parameter was 3 so we jump every 3 charact)
+#                   |  |  |  |  |  |  |  |  |  |  |  |  |
+#                   FavouriteMovieIsHarryPotterAndTheDeath
+#                   |  |  |  |  |  |  |  |  |  |  |  |  |
+#                   F  o  i  M  i  s  r  P  t  A  T  D  t
+
+# so the answer of str2[2:39:3] is FoiMisrPtATDt      
