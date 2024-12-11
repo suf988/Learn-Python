d265a24839ec4b49e046d20fd47f576aa6d504b8
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-11 12:16:40 +0100

    learned escape sequences

---
+++ Chapter_3/03_escape_sequence.py
@@ -0,0 +1,18 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ESCAPE SEQUENCE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#  \n   (for adding new line inside a string)
+print("Twinkle twinkle little star\nHow I wonder what you are")
+
+#  \t   (for adding a tab space(5 characters) inside a string)
+print("I lived in Pakistan earlier\tNow I live in Italy.")
+
+#  \"" (for adding "" in a string of "")
+#  \'' (for adding '' in a string of '')
+
+print("I am learning \"Python\" language")
+print('I am learning \'Python\' language')
+
+#  \\   (for adding a \ inside a string)
+print("Adding a backslash \ inside a string.")
