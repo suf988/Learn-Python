0fe29307e41003c89da99ee42a4fe999aa26bae5
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-12 16:05:02 +0100

    learning list methods

--- Chapter_4/02_list_methods.py
+++ Chapter_4/02_list_methods.py
@@ -29,6 +29,18 @@ print(l3)   #['apple', 'Banana', 'Cherry', 'date']
 # SORT METHOD (sorting in descending order)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# Syntax: list.sort(reverse=True)
+# using "reverse=True" inside the sort method will sort the list in descending order
+
+numbers = [5, 2, 9, 1, 7]
+numbers.sort(reverse=True)
+print(numbers) #[9, 7, 5, 2, 1]
+
+l4 = ["Banana", "apple", "Cherry", "date"]
+l4.sort(key=str.lower, reverse=True)
+print(l4)   #['date', 'Cherry', 'Banana', 'apple']
+
+
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # SORT METHOD 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
