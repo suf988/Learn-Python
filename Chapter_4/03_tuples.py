7d84d377259906569da5315f89d42d075a370b3b
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-28 15:49:16 +0100

    learning tuples in python

--- Chapter_4/03_tuples.py
+++ Chapter_4/03_tuples.py
@@ -3,5 +3,12 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # tuples are just like array/list but they are immutable. they are defined by ()
+
 tup1 = ()
 print(type(tup1))
+
+#tup2 = (1)  #this is the wrong way to enter one item in tuples, it will be read as 'int'
+tup2 = (1,) #tuple with only one value needs a comma
+
+tup3 = (32, 43.2, False, "Harry", "Hermione")
+print(f"tup3 is a {type(tup3)}, and its values are: {tup3}")
