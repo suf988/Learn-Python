e46d15e3b4f104bba1b4eb4891b6b9429fcbe8ec
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-28 17:49:39 +0100

    solved lists & tuples practice question 4

---
+++ Chapter_4_PS/04_problem4.py
@@ -0,0 +1,5 @@
+# Q4: Write a program to sum a list with 4 numbers.
+
+numberList = [32, 56.78, 98, 65.26]
+sumOfNumbers = sum(numberList)
+print(f"Sum of numbers in the list: {round(sumOfNumbers,2)}")   # here i have deliberately used round() function to round off the numbers to two digits after decimal.
