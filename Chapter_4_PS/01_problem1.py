6e31101cd897f952c83c8496c5a480482e454d2a
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-28 17:31:29 +0100

    solved lists & tuples practice question 1

---
+++ Chapter_4_PS/01_problem1.py
@@ -0,0 +1,11 @@
+# Q1: Write a program to store seven fruits in a list entered by the user.
+
+fruits = []
+fruits.append(str(input("Enter first fruit name: ")))
+fruits.append(str(input("Enter first second name: ")))
+fruits.append(str(input("Enter first third name: ")))
+fruits.append(str(input("Enter first fourth name: ")))
+fruits.append(str(input("Enter first fifth name: ")))
+fruits.append(str(input("Enter first sixth name: ")))
+fruits.append(str(input("Enter first seventh name: ")))
+print(f"Fruits: {fruits}")
