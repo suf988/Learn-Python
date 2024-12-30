ad3070ddc0a7d39f0f243351bbf93d2004d2d098
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-30 18:36:54 +0100

    solved dict & sets practice question 9

---
+++ Chapter_5_PS/09_problem9.py
@@ -0,0 +1,6 @@
+# Q9: Can you change the values inside a list which is contained in set S?
+
+s = {8, 7, 12, "Harry", [1,2]}
+
+# this code will raise an error because sets can only contain immutable elements and list is mutable.
+# so therefore, we can not change the values inside a list which is contained in a set.
