cbed0335d3e7c34c50593a449a1c84ec966ca37c
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-30 12:48:37 +0100

    learning sets in python

--- Chapter_5/03_sets.py
+++ Chapter_5/03_sets.py
@@ -12,3 +12,6 @@ print(type(emptySet))
 sets = {32, 41, 15, "Arif", False, 32, False, 20, 15}
 print(sets) # {32, False, 20, 'Arif', 15, 41}       repeated values are not stored.
 # sets are unordered, python does not maintain the sequence of the data entered. it is stored in random order
+
+# length of sets:
+print(len(sets))
