bd4f850b50b5e5459223380ca01ae2067c60e9ba
Sufyan Ahmed <sufyanahmed49@gmail.com>
2024-12-30 16:21:44 +0100

    solved dict & sets practice question 3

---
+++ Chapter_5_PS/03_problem.py
@@ -0,0 +1,7 @@
+# Q3: Can we have a set with 18 (int) and '18' (str) as a value in it?
+
+# Yes we can have set with 18(int) and '18'(str) as a value.
+# because both are different datatypes.
+
+aSet = {33, 21, 18, 50, "18"}
+print(f"Set: {aSet}")
