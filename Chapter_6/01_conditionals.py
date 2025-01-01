0db9d1d9229e839497d3a95d1c92ba7c68c108f6
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-01 18:37:59 +0100

    learning if else statements

--- Chapter_6/01_conditionals.py
+++ Chapter_6/01_conditionals.py
@@ -1,8 +1,6 @@
 age = int(input("Enter your age: "))
 
-if(age<=0):
-    print("Invalid age! Please enter a valid age")
-elif(age>=18):
+if(age>=18):
     print("You are above 18! You are good to go")
 else:
     print("Sorry! You are below 18")
