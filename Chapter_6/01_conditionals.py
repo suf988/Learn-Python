e92ecb3e85ea5d1aa4454789979bb8551899df5f
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-01 18:55:24 +0100

    learning if else statements

--- Chapter_6/01_conditionals.py
+++ Chapter_6/01_conditionals.py
@@ -1,6 +1,6 @@
 age = int(input("Enter your age: "))
 
 if(age>=18):
-    print("You are above 18! You are good to go")
+    print("You are above 18! You are good to go")   #the space before "print" is called "indent" or "indentation"
 else:
     print("Sorry! You are below 18")
