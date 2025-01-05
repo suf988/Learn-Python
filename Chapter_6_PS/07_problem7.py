5eab7ff6d2f5adbd19a724f736a61827157a3b10
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-05 11:54:01 +0100

    solved conditional statements practice question 7

--- Chapter_6_PS/07_problem7.py
+++ Chapter_6_PS/07_problem7.py
@@ -2,7 +2,18 @@
 
 post = input("Write a sentence: ").lower()
 
+print("*****METHOD 1*****")
 if(post.find("harry") != -1):
     print("Your post is talking about Harry.")
 else:
     print("Your post is not talking about Harry.")
+
+# OR
+
+print("*****METHOD 2*****")
+
+if("harry" in post):
+    print("Your post is talking about Harry.")
+else:
+    print("Your post is not talking about Harry.")
+    
