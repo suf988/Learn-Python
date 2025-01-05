7ec17e555e860044de4ea72ba9add6c164b0f326
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-05 12:08:28 +0100

    learning loops in python

---
+++ Chapter_7/01_loops.py
@@ -0,0 +1,18 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  LOOPS 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Loops are used to perform tasks repetitively without having to write them every time.
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# OR
+
+for i in range(1,6):
+    print(i)
+
+# Generally there are two types of loops: (for loop and while loop)
