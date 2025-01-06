cba3b444193f907d7807d812e0c27b95d8396183
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-06 19:30:05 +0100

    learning pass function in for loop

--- Chapter_7/05_break_continue_and_pass.py
+++ Chapter_7/05_break_continue_and_pass.py
@@ -21,3 +21,20 @@ for j in range(9):
     if(j == 4):
         continue    #when j == 6, the loop will skip only that iteration and will move on to next iteration.
     print(j)
+
+print("====================================")
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  PASS IN FOR LOOP
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# "pass" is a null statement, which means to do nothing.
+# when you delare  a loop and want to do nothing in it and continue to work further, we use "pass" in that loop.
+
+for k in range(5):
+    pass    #this "pass" will nullify this loop and allow the further code to execute without error.
+
+val = 0
+while(val<3):
+    print(val)
+    val += 1
