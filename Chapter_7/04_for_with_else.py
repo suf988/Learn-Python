bd69bb5cff477a460e7e2475ee5807ee8c79f3e0
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-06 19:04:25 +0100

    learning for loop with else

---
+++ Chapter_7/04_for_with_else.py
@@ -0,0 +1,10 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  FOR LOOP WITH ELSE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# for loop also comes with an optional "else" statement which is executed when the loop is exhausted(ended).
+
+for i in range(5):
+    print(i)
+else:
+    print("Loop is ended")
