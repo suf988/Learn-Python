1bc375dce1c798725f9e1b67c32d40847b99202f
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-07 19:02:29 +0100

    solved loops practice question 12

---
+++ Chapter_7_PS/12_problem12.py
@@ -0,0 +1,6 @@
+# Q12: Write a program to print multiplication table of n using for loops in reversed order.
+
+n = int(input("Enter a number: "))
+
+for i in range(10, 0, -1):
+    print(f"{n} x {i} = {n*i}")
