47ced0fbfb0765e1dd7084cf3c4012b8114ed626
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-08 12:32:15 +0100

    solved a quick quiz

---
+++ Chapter_8/02_quick_quiz.py
@@ -0,0 +1,8 @@
+# Quick Quiz: Write a program to greet a user with “Good day” using functions.
+
+def greet():
+    name = input("Enter your name: ")
+
+    print(f"Hello, have a good day {name}")
+
+greet()
