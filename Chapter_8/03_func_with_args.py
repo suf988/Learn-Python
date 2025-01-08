cf1e4089e01941007c1c6cb9462ddd5e20ae36d7
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-08 13:19:24 +0100

    learning function with arguements and return statement

--- Chapter_8/03_func_with_args.py
+++ Chapter_8/03_func_with_args.py
@@ -1,10 +1,10 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  FUNCTIONS WITH ARGUEMENTS
+#  FUNCTIONS WITH ARGUMENTS
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # a function can accept some values when it is called and those values can be used or executed inside the function.
 
-def func1(name, greeting):  #this function is taking to arguements and using them inside the function.
+def func1(name, greeting):  #this function is taking to arguments and using them inside the function.
     print(f"{greeting} {name}, how are you today?")
 
 func1("Harry", "Good morning")  #Good morning Harry, how are you today?
@@ -26,6 +26,7 @@ def func2(num1, num2):
 
 a = func2(23, 56)
 b = func2(69, 87)
+
 print(f"The first average of two numbers is: {a:.2f}")
 print(f"The second average of two numbers is: {b:.2f}")
 print(f"The sum of the two averages is: {a+b}")
