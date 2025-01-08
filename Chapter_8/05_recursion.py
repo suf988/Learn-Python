7150155258ef6248e91f7a6cee78e736c86ad5ed
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-08 15:56:59 +0100

    learning recursion with factorial function example

--- Chapter_8/05_recursion.py
+++ Chapter_8/05_recursion.py
@@ -43,17 +43,17 @@ Here's what the stack looks like as the program runs:
 
 While Recursing (pushing to stack):
 
-factorial(5) → Waiting for factorial(4)
-factorial(4) → Waiting for factorial(3)
-factorial(3) → Waiting for factorial(2)
-factorial(2) → Waiting for factorial(1)
-factorial(1) → Base case reached!
+factorial(5)    →   Waiting for factorial(4)
+factorial(4)    →   Waiting for factorial(3)
+factorial(3)    →   Waiting for factorial(2)
+factorial(2)    →   Waiting for factorial(1)
+factorial(1)    →   Base case reached!
 
 
 During Unwinding (popping from stack):
 
-factorial(1) returns 1 → factorial(2) calculates 2 * 1 = 2
-factorial(2) returns 2 → factorial(3) calculates 3 * 2 = 6
-factorial(3) returns 6 → factorial(4) calculates 4 * 6 = 24
-factorial(4) returns 24 → factorial(5) calculates 5 * 24 = 120
+factorial(1)    returns 1   →   factorial(2)    calculates  2 * 1 = 2
+factorial(2)    returns 2   →   factorial(3)    calculates  3 * 2 = 6
+factorial(3)    returns 6   →   factorial(4)    calculates  4 * 6 = 24
+factorial(4)    returns 24  →   factorial(5)    calculates  5 * 24 = 120
 '''
