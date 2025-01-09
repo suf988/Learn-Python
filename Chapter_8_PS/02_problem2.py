55134a870f93d5a4ed25b7c95ea9d00c9b89f2cc
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-09 12:29:10 +0100

    solved functions practice question 2

--- Chapter_8_PS/02_problem2.py
+++ Chapter_8_PS/02_problem2.py
@@ -1,16 +1,16 @@
 # Q2: Write a python program using function to convert Celsius to Fahrenheit.
 
-def temp_conversion():
-    sel_temp = input("Type 'F' to convert to Farenheit or 'C' to convert to Celcius: ").lower()
+def temp_conversion(sel_temp):
 
     if(sel_temp == "f"):
         temp = float(input("Enter temperature in Celcius: "))
-        print(f"{temp}°C into Farenheit is: {(temp * 9/5) + 32:.2f}°F")
+        return f"{temp}°C into Farenheit is: {(temp * 9/5) + 32:.2f}°F"
     elif(sel_temp == "c"):
         temp = float(input("Enter temperature in Farenheit: "))
-        print(f"{temp}°F into Celcius is: {(temp - 32) * 5/9:.2f}°C")
+        return f"{temp}°F into Celcius is: {(temp - 32) * 5/9:.2f}°C"
     else:
-        print("Error! Please enter a valid conversion.")
-        temp_conversion()
+        return "Error! Please enter a valid conversion."
     
-temp_conversion()
+sel_temp = input("Type 'F' to convert to Farenheit or 'C' to convert to Celcius: ").lower()
+temp = temp_conversion(sel_temp)
+print(temp)
