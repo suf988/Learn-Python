54ffe19d29213f386a521bc4ee5f2457dd1c47ca
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 13:47:18 +0100

    modified file appending code

--- Chapter_9/03_file_append.py
+++ Chapter_9/03_file_append.py
@@ -4,6 +4,7 @@
 
 # append() method is used to append the new text at the end of the file.
 # only new data is added to the file, you can not overwrite the existing content of the file.
+# will create a new file if the file doesn't exist.
 
 newString = "\nThis line is appended in the this txt file using the append mode"
 
