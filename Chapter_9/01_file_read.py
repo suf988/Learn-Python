2e8daf5aefcd8720a2bf7ab2b384ea229ce05049
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 12:52:08 +0100

    learning reading file using python

--- Chapter_9/01_file_read.py
+++ Chapter_9/01_file_read.py
@@ -9,3 +9,21 @@ data = f.read()         # read() function can read the material in the file.
 print(data)
 
 f.close()               #close() function is used to specify that the work has been done and the file is closed.
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  READLINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# readlines will read all the lines individually and return a list of each line in that file.
+
+f = open("file.txt")
+
+data = f.readlines()
+
+print(data, type(data))
+
+f.close()
+
+# OUTPUT:
+
+# ['Twinkle twinkle little star\n', '\n', 'How I wonder what you are\n', '\n', 'Up, above the world so high\n', '\n', 'Like a diamond in the sky'] <class 'list'>
