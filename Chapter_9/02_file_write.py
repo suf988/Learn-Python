79f84bb83a980aa5223aae0968e2ffae72bbc8d8
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 13:19:46 +0100

    learning writing a file using python

--- Chapter_9/02_file_write.py
+++ Chapter_9/02_file_write.py
@@ -1,3 +1,10 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  WRITE FILE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# write() method is used to write some text into a file.
+# using "w" mode will create a new fiel adn write the given text into that file.
+
 f = open("written_file.txt", "w")   #2nd parameter is given to specify the purpose for which the file is opened.
 
 f.write("This is a text file written by using f.write() function")
