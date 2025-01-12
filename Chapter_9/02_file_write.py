3c412d64ebcaff9ed9f81d9c2f3d69fc85b5c8be
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 13:09:02 +0100

    learning writing a file using python

---
+++ Chapter_9/02_file_write.py
@@ -0,0 +1,5 @@
+f = open("written_file.txt", "w")   #2nd parameter is given to specify the purpose for which the file is opened.
+
+f.write("This is a text file written by using f.write() function")
+
+f.close()
