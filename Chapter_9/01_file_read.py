adfa2451032ebc09d85bddd160868a79080dba62
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 13:00:18 +0100

    learning reading file using python

--- Chapter_9/01_file_read.py
+++ Chapter_9/01_file_read.py
@@ -2,6 +2,8 @@
 #  READ FILE
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+# read() will read the whole txt file as a single string
+
 f = open("file.txt")    #open() is a built-in python function to tell that is file is opened for some work.
 
 data = f.read()         # read() function can read the material in the file.
@@ -11,7 +13,7 @@ print(data)
 f.close()               #close() function is used to specify that the work has been done and the file is closed.
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  READLINES
+#  READLINES FUNCTION
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # readlines will read all the lines individually and return a list of each line in that file.
@@ -27,3 +29,17 @@ f.close()
 # OUTPUT:
 
 # ['Twinkle twinkle little star\n', '\n', 'How I wonder what you are\n', '\n', 'Up, above the world so high\n', '\n', 'Like a diamond in the sky'] <class 'list'>
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  READLINE FUNCTION
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# readline() reads the text line-by-line, means one line at a time. and then moves to the next line and read it.
+
+f = open("file.txt")
+
+data = f.readline() # will read only a single line from the file
+
+print(data) #twinkle twinkle little star
+
+f.close()
