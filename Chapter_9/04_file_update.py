da1f84e5310f2ef5d5832a9a3ca129e3891137a6
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-12 14:01:36 +0100

    updated a txt file using update (w+) mode

--- Chapter_9/04_file_update.py
+++ Chapter_9/04_file_update.py
@@ -17,3 +17,13 @@ f.close()
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #  UPDATE FILE (w+)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# used to read and update an existing file.
+# will create a new file if the file doesn't exist
+# all the existing content will be deleted and the updated content you entered will be stored in the file.
+
+f = open("written_file.txt", "w+")
+
+f.write("The file is updated with the new content and all the previous existing content is deleted because we have used update method (w+).")
+
+f.close()
