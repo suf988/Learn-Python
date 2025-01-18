33ad63ab7bcc28778b218f8401bb315269c55bca
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-18 18:56:31 +0100

    solved file i/o problem 7

--- Chapter_9_PS/07_problem7.py
+++ Chapter_9_PS/07_problem7.py
@@ -9,11 +9,17 @@ with open("log_file.html") as f:
     if 'python' in data:
         print(f"Keyword 'python' is present in line no. {line}")
     else:    
+        line += 1
         while(data != ""):
             if 'python' in data:
                 print(f"Keyword 'python' is present in line no. {line}")
+                break
             line += 1
             data = f.readline()
+        else:
+            print("Keyword 'python' is not present.")
+            
+
 
 # =================================================================
 
@@ -25,5 +31,7 @@ with open("log_file.html") as f:
     for i in data:
         if 'python' in i:
             print(f"Keyword 'python' is present in line no. {line}")
+        else:
+            print("Keyword 'python' is not present.")
         line += 1
-            
+    
