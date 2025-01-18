c7b22415fbac207b4360bac9f3a7cee1474dd837
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-18 16:08:53 +0100

    solved file i/o problem 3

---
+++ Chapter_9_PS/03_problem3.py
@@ -0,0 +1,12 @@
+# Q3: Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13 – year old.
+
+def multiply():
+    for i in range(2,21):
+        with open(f"table of {i}", 'w') as f:
+
+            f.write(f"Table of {i}\n")
+            for j in range(1,11):
+                f.write(f"\n{i} x {j} = {i*j}")
+    print("Successfully stored multiplication tables of 2-20 in different seperate files")
+
+multiply()
