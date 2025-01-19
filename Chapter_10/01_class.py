1258529e255fd86a236b735fa0c3ebf511a34c21
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-19 16:41:48 +0100

    learning oop in python

---
+++ Chapter_10/01_class.py
@@ -0,0 +1,6 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  OBJECT ORIENTED PROGRAMMING OOP
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# solving the problem by creating objects is one of the most famous approaches in programming.
+# The concept focuses on DRY principle (Donot Repeat Yourself) - using reusable code
