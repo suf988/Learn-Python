a4830f5bc57a06cbeb47e351287c7ae6d06c4286
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-23 17:43:27 +0100

    learing multi-level inheritance in OOP

---
+++ Chapter_11/03_multi_level_inheritance.py
@@ -0,0 +1,30 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  MULTI LEVEL INHERITANCE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# here, a class which is child itself, becomes parent for its child class
+
+#   PARENT =>   CHILD =>    CHILD
+
+class Animal:
+    adjective = "cute"
+
+    def __init__(self, name):
+        self.name = name
+    
+    def sound(self):
+        print(f"{self.adjective} {self.name} makes a sound.")
+
+class Mammal(Animal):
+    
+    def sound(self):
+        print(f"{self.name} is a {self.adjective} mammal and makes a specific sound.")
+
+class Dog(Mammal):
+    pass
+    def sound(self):
+        print(f"{self.adjective} {self.name} barks.")
+
+dog = Dog("Buddy")
+
+dog.sound()
