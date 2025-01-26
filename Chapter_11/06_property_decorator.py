9de9a3b1ca2b015311afa3d923b94d2098d0f6f8
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-26 16:44:43 +0100

    learning property decorator in oop

--- Chapter_11/06_property_decorator.py
+++ Chapter_11/06_property_decorator.py
@@ -2,23 +2,48 @@
 #  PROPERTY DECORATOR
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# consider the following code: (without property decorator)
-
+# Example without property decorator
 class Employee:
     def __init__(self, salary):
         self.salary = salary
     
-    def get_salary(self):
+    def get_salary(self):  # Getter method to retrieve salary
         return f"Salary: {self.salary}"
     
-    def set_salary(self, value):
+    def set_salary(self, value):  # Setter method to modify salary
         if value < 0:
             raise ValueError("Invalid input! Salary can't be negative.")
         self.salary = value
 
 emp = Employee(5000)
-print(emp.get_salary()) # method had to be called to get the value
+print(emp.get_salary())  # Method explicitly called to get the salary
 
-emp.set_salary(10000)   # method had to be called to set the value
+emp.set_salary(10000)   # Method explicitly called to set the salary
 print(emp.get_salary())
 
+'''
+The above code works, but explicitly calling getter and setter methods is less clean and makes the code less readable.
+
+We can use @property decorators to make the code cleaner, allowing attribute-style access for getting and setting values.
+'''
+
+# Example with @property decorator
+class Employee2:
+    def __init__(self, salary):
+        self._salary = salary  # Internal attribute for storing salary
+    
+    @property  # Getter method to retrieve salary
+    def salary(self):
+        return f"Salary: {self._salary}"
+    
+    @salary.setter  # Setter method to modify salary
+    def salary(self, value):
+        if value < 0:
+            raise ValueError("Invalid input! Salary can't be negative.")
+        self._salary = value
+
+emp2 = Employee2(15000)
+print(emp2.salary)  # Accessing the salary like an attribute
+
+emp2.salary = 20000  # Setting salary like an attribute
+print(emp2.salary)
