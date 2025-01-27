0c1ddcfe207f1a2dc384ea53addea01a99a65b38
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-01-27 12:38:36 +0100

    solved multiplication part in practice question 5

--- Chapter_11_PS/05_problem5.py
+++ Chapter_11_PS/05_problem5.py
@@ -7,11 +7,20 @@ class Complex:
 
     def __add__(self, other):
         return Complex(self.r + other.r, self.i + other.i)
+
+    def __mul__(self, other):
+        real_part = self.r * other.r - self.i * other.i
+        imaginary_part = self.r * other.i + self.i * other.r
+        return Complex(real_part, imaginary_part)
     
     def __str__(self):
-        return f"{self.r} + {self.i}i"
+        return f"{self.r} + {self.i}i" 
+    
+    def __str(self):
+        return f"{self.r} * {self.i}i"
     
 c1 = Complex(1, 2)
 c2 = Complex(3, 4)
 
-print(c1 + c2)
+print(f"Sum of two complex numbers: {c1 + c2}")
+print(f"Product of two complex numbers: {c1 * c2}")
