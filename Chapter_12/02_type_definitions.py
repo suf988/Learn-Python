09a36417caf70256c2cc06e639ce23c5b0e74612
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-06 12:21:59 +0100

    learning advance type hints in advance python

--- Chapter_12/02_type_definitions.py
+++ Chapter_12/02_type_definitions.py
@@ -18,5 +18,25 @@ a: int = 15    # using type definition
 name = "harry"       # normal way to write
 name: str = "harry"  # using type definition
 
+def func1(a: int, b: int)-> int:
+    return a+b
+
+result = func1(5,2)
+print(result)
+
 # ============================================================
 
+from typing import List, Tuple, Dict, Union
+
+# List of integers
+numbers: List[int] = [1, 2, 3, 4, 5]
+
+# Tuple of a string and an integer
+person: Tuple[str, int] = ("Alice", 30)
+
+# Dictionary with string keys and integer values
+scores: Dict[str, int] = {"Alice": 90, "Bob": 85}
+
+# Union type for variables that can hold multiple types
+identifier: Union[int, str] = "ID123"
+identifier = 12345 # Also valid
