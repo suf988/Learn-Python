e4672d40304b0aadca90b366532da1a28e052381
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-06 12:18:11 +0100

    learning type-definitions in advance python

---
+++ Chapter_12/02_type_definitions.py
@@ -0,0 +1,22 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#  TYPE DEFINITIONS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# type-definitions or type-hints is a way to make the code more documented by defining their types at the time of the declaration of variables or functions.
+
+'''
+SYNTAX:
+
+FOR VARIABLES:      a : datatype = value
+FOR FUNCTIONS:      def func(a: datatype ,b: datatype) -> returnDatatype:
+
+'''
+
+a = 15         # normal way to write
+a: int = 15    # using type definition
+
+name = "harry"       # normal way to write
+name: str = "harry"  # using type definition
+
+# ============================================================
+
