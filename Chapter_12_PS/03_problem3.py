b2eba6aced0574262ff2b63b9c5b0dc59f52bccd
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-10 12:21:20 +0100

    solved advance python practice question 3

---
+++ Chapter_12_PS/03_problem3.py
@@ -0,0 +1,6 @@
+# Q3: Write a list comprehension to print a list which contains the multiplication table of a user entered number.
+
+number = int(input("Enter a number to get multiplication table: "))
+table = [number*i for i in range(1,11) ]
+
+print(table)
