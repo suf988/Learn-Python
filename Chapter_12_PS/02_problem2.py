558ac43c58c0c0188df4fb2649e784857cf4f866
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-10 18:28:29 +0100

    solved advance python practice question 2

--- Chapter_12_PS/02_problem2.py
+++ Chapter_12_PS/02_problem2.py
@@ -2,6 +2,4 @@
 
 fruits = ['apple', 'banana', 'grapes', 'mango', 'blueberry', 'orange', 'strawberry', 'pear', 'pineapple']
 
-for index, item in enumerate(fruits, start=1):
-    if index == 3 or index == 5 or index == 7:
-        print(f"Item number{index}: {item}")
+
