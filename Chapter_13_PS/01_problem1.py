957ad965e82978651f8164ee329e9b8077070fe3
Sufyan Ahmed <sufyanahmed49@gmail.com>
2025-02-13 13:56:54 +0100

    solved advanced python practice question 1

---
+++ Chapter_13_PS/01_problem1.py
@@ -0,0 +1,7 @@
+# Q1: Write a program to input name, marks and phone number of a student and format it using the format function like below:
+
+# “The name of the student is Harry, his marks are 72 and phone number is 99999888”
+
+string = "The name of the student is {}, his marks are {} and phone number is {}"
+
+print(string.format("Harry","72", "99999888"))
